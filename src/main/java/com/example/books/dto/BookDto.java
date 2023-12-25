af906e0888a59c70efbd18622f5f3c83f38c9a60
Dmytro Zarudnyi <dmytro.zarudnyi@gmail.com>
2023-12-25 22:49:55 +0200

    fixed some validations and errors after revert

--- src/main/java/com/example/books/dto/BookDto.java
+++ src/main/java/com/example/books/dto/BookDto.java
@@ -1,18 +1,21 @@
 package com.example.books.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class BookDto {
     private Long id;
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
     @NotNull
+    @ISBN(type = ISBN.Type.ANY)
     private String isbn;
     @NotNull
     @Min(0)
--- src/main/java/com/example/books/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/books/dto/CreateBookRequestDto.java
@@ -1,17 +1,20 @@
 package com.example.books.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
-    @NotNull
+    @NotBlank
     private String author;
     @NotNull
+    @ISBN(type = ISBN.Type.ANY)
     private String isbn;
     @NotNull
     @Min(0)
